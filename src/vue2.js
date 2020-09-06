47d783a7e3527751b67eddd9479a44e03d463b24
chang_su <chang_su@hunliji.com>
2020-09-06 11:13:11 +0800

    textUpdator

--- src/vue2.js
+++ src/vue2.js
@@ -4,6 +4,9 @@ class Watcher {
     this.key = key
     this.updateFn = updateFn
     Dep.target = this
+    // 触发上述依赖收集
+    this.vm[key]
+    Dep.target = null
   }
 
   update() {
@@ -56,14 +59,36 @@ class Compile {
 
   compileText = (node) => {
     const key = RegExp.$1
-    node.textContent = this.vm[key]
-    Dep.target = null
+    this.update(node, this.vm[key], 'text')
     new Watcher(this.vm, key, (val) => {
-      node.textContent = val
+      this.update(node, val, 'text')
     })
-    this.vm[key]
   }
-  compileElement = (node) => {}
+  compileElement = (node) => {
+    console.log('compileElement', node.attributes)
+    Array.from(node.attributes).forEach((attr) => {
+      if (attr.name.startsWith('v-')) {
+        const type = attr.name.substring(2)
+        const fn = this[type]
+        fn && fn(node, this.vm[attr.value])
+        new Watcher(this.vm, attr.value, (val) => {
+          fn && fn(node, val)
+        })
+      }
+    })
+  }
+
+  text = (node, val) => {
+    this.update(node, val, 'text')
+  }
+
+  textUpdator = (node, val) => {
+    node.textContent = val
+  }
+
+  update = (node, val, type) => {
+    this[`${type}Updator`](node, val)
+  }
 }
 
 class Vue {
